7dd72f116304f92c1aec0005b8c72fcea3cb3fef
aabccd021 <aabccd021@gmail.com>
2025-01-18 20:21:57 +0700

    - feat: Add initial project files and configurations for freeze-js

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # freeze-js
+
+- use esbuild to polyfill `Array.from`
+- localhost:8080
+- handle typescript
+- innerhtml instead of outerhtml
+- support non module scripts
+- assert `/static` is not in sessionStorage
+- only dispatch event unsubscribe
---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = { self, nixpkgs, treefmt-nix }:
+
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs.prettier.enable = true;
+        programs.nixpkgs-fmt.enable = true;
+        programs.biome.enable = true;
+        programs.shfmt.enable = true;
+        settings.formatter.prettier.priority = 1;
+        settings.formatter.biome.priority = 2;
+      };
+
+    in
+
+    {
+
+      checks.x86_64-linux = {
+        formatting = treefmtEval.config.build.check self;
+      };
+
+      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+
+      devShells.x86_64-linux.default = pkgs.mkShellNoCC {
+        shellHook = ''
+          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers-chromium}
+        '';
+        buildInputs = [
+          pkgs.nodejs
+          pkgs.biome
+          pkgs.typescript
+        ];
+      };
+
+    };
+}
