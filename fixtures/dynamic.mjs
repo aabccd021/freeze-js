bb557a0edc3cd2d4b45fda0ed64e46aac60a4867
aabccd021 <aabccd021@gmail.com>
2025-01-24 20:11:54 +0700

    1. Fix setting dataset property correctly in freezePageLoad() 2. Refactor static.mjs for better event handling

--- fixtures/dynamic.mjs
+++ fixtures/dynamic.mjs
@@ -1,7 +1,7 @@
 export async function freezePageLoad() {
   const h1 = document.createElement("h1");
   h1.textContent = "H1Dy";
-  h1.dataset.testid = "main";
+  h1.dataset["testid"] = "main";
   document.body.appendChild(h1);
 
   await new Promise((resolve) => setTimeout(resolve, 10));
--- fixtures/static.mjs
+++ fixtures/static.mjs
@@ -1,3 +1,6 @@
-document.querySelector("[data-testid=main]").addEventListener("click", () => {
-  console.warn("click static");
-});
+const mainElt = document.querySelector("[data-testid=main]");
+if (mainElt !== null) {
+  mainElt.addEventListener("click", () => {
+    console.warn("click static");
+  });
+}
